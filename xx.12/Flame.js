3fd70a54cccd0b465208bd987245b10e8e9597f6
George makulu <59498844+gmright2@users.noreply.github.com>
2020-03-27 03:08:11 -0500

    Create Flame.js

---
+++ xx.12/Flame.js
@@ -0,0 +1,47 @@
+Loops-open/guide-checklists 
+ Loosening:*action*
+Loose:slideshow 
+ Gmright2-province/jobs
+QA-loops/proactive 
+   Loops-> "log"
+Wrote-butket-QA 
+   In-predator 
+ Loops=netdata 
+Loops_/%-gmright2 
+Loops_clock 
+Loops/gmright2-book 
+   Flame:(4.6.7)
+Flame:(7.8.1)
+Flame:(1.9.0)
+Flame-workflow/git/repostory 
+Flame_job
+Flame-loops
+Flame:(1.0.1)
+Flame:(1.1.1)
+Flame:(0.1.1)
+Flame:(3.2.1)
+Flame>flexibility 
+Flame:$path
+Flame_in/right_duration 
+
+MIT License
+
+Copyright (c) 2020 gmright2
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
